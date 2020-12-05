a1532c7ea771c11209f1f461cf0c6e3bf2c2ee97
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2020-12-06 06:16:00 +0700

    Gist dom for option and visibility

--- source/upload/admin/model/extension/module/architect.php
+++ source/upload/admin/model/extension/module/architect.php
@@ -431,6 +431,7 @@ class ModelExtensionModuleArchitect extends Model
             'modification'     => $this->getDOMTag($dom, 'modification'),
             'event'            => $this->getDOMTag($dom, 'event'),
             'admin_controller' => $this->getDOMTag($dom, 'admin_controller'),
+            'option'           => json_decode($this->getDOMTag($dom, 'option'), true),
         );
 
         return $data;
